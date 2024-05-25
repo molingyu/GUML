1b19c2f5686de6e99619bf15e5cb1a1407fb708e
molingyu <z1622716486@hotmail.com>
2024-05-25 23:57:30 +0800

    feat: update test application

---
+++ TestApplication/scripts/controllers/SettingController.cs
@@ -0,0 +1,14 @@
+using GUML;
+
+public class SettingController : GuiController
+{
+    public override void Update()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void Dispose()
+    {
+        throw new System.NotImplementedException();
+    }
+}
